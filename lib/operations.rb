3d053c61a0771242d83e212ac8e355e935852278
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-11 17:11:03 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,9 @@
 def unsafe?(speed)
-  if speed > 60
-	  return true
-	elsif speed < 40
-	  return true
-	elsif speed == 50
-	  return false
-	end
+  if speed < 40 || speed > 60
+    return true
+  else
+    return false
+  end
 end
 
 
